e781e1eddfcf67e6749d935cdf853a52dc8fbf8a
Sloane Vaznova <sloane.vaznova@gmail.com>
2024-09-08 19:42:18 -0500

    Remove `routes`.
    Site only planned to be one page. Will re-implement routing later if
    necessary.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   template: ``,
 })
 export class AppComponent {
