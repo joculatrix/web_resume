cb40956708401ba104020603698651ac4e5a6c86
Sloane Vaznova <sloane.vaznova@gmail.com>
2024-09-11 19:21:56 -0500

    Refactor and revise `about`.

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { bio } from './data/bio';
 import { tidbits } from './data/tidbits';
 
 @Component({
@@ -9,15 +10,6 @@ import { tidbits } from './data/tidbits';
     templateUrl: './templates/about.component.html',
 })
 export class AboutComponent {
-    name = 'Sloane Vaznis';
-    pronouns = ['she', 'her'];
-    // I'm preferring an ES5-style multiline string here vs a template because I think
-    // the indentation lining up looks prettier:
-    bio = 'I\'m a passionate learner and creator, with a background in ' +
-          'programming, music composition and performance, visual art, ' +
-          'writing, and poetry. Absorbing new skills and techniques brings ' +
-          'me a unique satisfaction. I love to learn about abtract ' +
-          'and theoretical topics! I am currently looking for work as a ' +
-          'software developer or site reliability engineer.';
+	bio = bio;
     tidbits = tidbits.map(tidbit => [Object.keys(tidbit)[0], Object.values(tidbit)[0]]);
 }
---
+++ src/app/data/bio.ts
@@ -0,0 +1,18 @@
+type Bio = {
+	name: string;
+	pronouns: [string, string];
+	desc: string;
+}
+
+export const bio: Bio = {
+	name: 'Sloane Vaznis',
+	pronouns: ['she', 'her'],
+	desc:
+		'I\'m a passionate learner and creator, and besides programming, I have a background in ' +
+		'music composition and performance, visual art, writing, and poetry. I am rarely not absorbing ' +
+		'some new skill, or information on some new topic. The world, to me, is full of endless ' +
+		'interesting and wonderful things. Why not master all of it? I love to learn about and discuss ' +
+		'abstract and theoretical topics, functional programming features, and ways to make programs ' +
+		'or algorithms more clean and concise. Currently, I am looking for work as a developer or ' +
+		'site reliability engineer.'
+}
